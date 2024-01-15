489c10859974e200894a5a6a25e1ae0f52fd8178
Maksin Vasilii <Kimano.arb@mail.ru>
2024-01-15 20:55:58 +0500

    save new format

--- main.py
+++ main.py
@@ -43,3 +43,12 @@ def process_contacts(contacts):
                 existing_contact[4] = contact[4]
 
     return list(unique_contacts.values())
+
+
+
+import csv
+
+def save_contacts_to_csv(contacts, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(contacts)
